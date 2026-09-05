365edda517589194fbc509e14c4c77f356554b4d
sureshbaviskar07-del <sureshbaviskar07@gmail.com>
2026-09-05 11:40:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-// Suresh Jobs - Supabase Configuration
-
 window.SJ_CONFIG = {
   SUPABASE_URL: "https://uesqzucgpnpvjtyxvx.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_XXXXXXXXXXXX"
+  SUPABASE_ANON_KEY: "sb_publishable_6KikA5n7vp6yeTNbcGNpww_apR51KCL"
 };
